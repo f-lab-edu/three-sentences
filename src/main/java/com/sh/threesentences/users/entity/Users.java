f76cfe4e92aae9d757fd1e75b47d0a9aacf33e91
shinsanghooon <shinsanghooon@gmail.com>
2023-01-26 01:48:51 +0900

    feature[#7]: 사용자 엔티티 생성
    - 사용자는 이메일, 이름 ,패스워드, 멤버십 값을 가진다.
    - 멤버십은 초기에 FREE로 세팅된다.

--- src/main/java/com/sh/threesentences/users/entity/Users.java
+++ src/main/java/com/sh/threesentences/users/entity/Users.java
@@ -1,5 +1,6 @@
 package com.sh.threesentences.users.entity;
 
+import com.sh.threesentences.users.dto.UserResponseDto;
 import com.sh.threesentences.users.enums.MembershipType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,25 +12,55 @@ import javax.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 사용자 엔티티
+ */
 @Entity
 @Getter
 @NoArgsConstructor
 public class Users {
 
+    /**
+     * 엔티티 식별자
+     */
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    /**
+     * 사용자 이메일
+     */
+    @Column(unique = true)
     private String email;
 
+    /**
+     * 사용자 이름
+     */
     @Column
     private String name;
 
+    /**
+     * 사용자 패스워드
+     */
     @Column
     private String password;
 
+    /**
+     * 사용자 멤버십 타입
+     * FREE, BASIC, PREMIUM 3가지 타입이 존재
+     * 초기 디폴드값은 FREE로 세팅
+     */
     @Column
     @Enumerated(EnumType.STRING)
     private MembershipType membership = MembershipType.FREE;
 
+    public Users(String email, String name, String password, MembershipType membership) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.membership = membership;
+    }
+
+    public UserResponseDto toUserResponseDto() {
+        return new UserResponseDto(this.id, this.email, this.name, this.membership);
+    }
 }
