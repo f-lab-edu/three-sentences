f7bb50dbb1540ada154f149ad6beec51f18e0aae
shinsanghooon <shinsanghooon@gmail.com>
2023-01-27 01:18:29 +0900

    refactor: UserRequestDto 필드에 final 키워드 추가
    - `@RequiredArgsConstructor`에 의해 생성자를 만들어지는 것을 의도하여 추가하였음

--- src/main/java/com/sh/threesentences/users/dto/UserRequestDto.java
+++ src/main/java/com/sh/threesentences/users/dto/UserRequestDto.java
@@ -15,17 +15,17 @@ public class UserRequestDto {
 
     @NotBlank(message="메일을 입력해주세요.")
     @Email(message = "이메일 형식이 잘못되었습니다.")
-    private String email;
+    private final String email;
 
     @NotBlank(message="이름을 입력해주세요.")
     @Size(max=20, message="이름의 최대 길이는 20자 입니다.")
-    private String name;
+    private final String name;
 
     @NotNull(message = "패스워드를 입력해주세요.")
     @NotBlank(message="패스워드를 입력해주세요.")
-    private String password;
+    private final String password;
 
-    private MembershipType membership;
+    private final MembershipType membership;
 
     public Users toEntity(String encryptedPassword) {
         return Users.builder()
