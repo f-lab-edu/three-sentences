bd562e2968501045bbe56dff9aecbc93376b8fc0
shinsanghooon <shinsanghooon@gmail.com>
2023-02-04 14:29:11 +0900

    feature: EnumType Validation 추가

--- src/main/java/com/sh/threesentences/common/exception/CommonExceptionHandler.java
+++ src/main/java/com/sh/threesentences/common/exception/CommonExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sh.threesentences.common.exception;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.sh.threesentences.common.dto.ErrorResponseDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,8 +15,19 @@ public class CommonExceptionHandler {
 
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(code = BAD_REQUEST)
-    public ErrorResponseDto handleEmailDuplicateException(IllegalStateException ex) {
+    public ErrorResponseDto handleIllegalStatesException(IllegalStateException ex) {
         return new ErrorResponseDto(ex.getMessage());
+    }
+
+    /**
+     * 입력값이 ENUM에 포함되지 않는 경우, 예외를 던집니다.
+     *
+     * @return 메시지가 담긴 응답
+     */
+    @ExceptionHandler(InvalidFormatException.class)
+    @ResponseStatus(code= BAD_REQUEST)
+    public ErrorResponseDto handleMethodArgumentNotValid() {
+        return new ErrorResponseDto("입력 값이 유효하지 않습니다. ");
     }
 
 }
