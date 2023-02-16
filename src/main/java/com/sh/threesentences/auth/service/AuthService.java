5a9c27bb9c9b9f7df37392a284f18ddcc62663ca
shinsanghooon <shinsanghooon@gmail.com>
2023-02-17 01:34:50 +0900

    refactor: Transactional readonly 옵션 추가

--- src/main/java/com/sh/threesentences/auth/service/AuthService.java
+++ src/main/java/com/sh/threesentences/auth/service/AuthService.java
@@ -2,16 +2,16 @@ package com.sh.threesentences.auth.service;
 
 
 import com.sh.threesentences.auth.dto.LoginDto;
-import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AuthService {
 
