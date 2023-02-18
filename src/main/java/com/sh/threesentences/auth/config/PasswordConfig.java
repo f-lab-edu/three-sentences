25087fb185d1bda06325750cdf994befd32ad200
shinsanghooon <shinsanghooon@gmail.com>
2023-02-19 00:35:31 +0900

    refactor: PasswordEncoder를 PasswordConfig로 이동

---
+++ src/main/java/com/sh/threesentences/auth/config/PasswordConfig.java
@@ -0,0 +1,21 @@
+package com.sh.threesentences.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    /**
+     * 사용자 패스워드 저장 및 인증에 사용되는 Bean
+     *
+     * @return PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(4);
+    }
+
+}
