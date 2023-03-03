9faa1797bf16fe0c74cce272d6492706a82d8d56
shinsanghooon <shinsanghooon@gmail.com>
2023-03-04 01:17:13 +0900

    refactor: 가독성을 떨어뜨리는 줄바꿈 수정

--- src/main/java/com/sh/threesentences/users/service/AuthorityService.java
+++ src/main/java/com/sh/threesentences/users/service/AuthorityService.java
@@ -3,6 +3,7 @@ package com.sh.threesentences.users.service;
 import static com.sh.threesentences.readingspace.exception.ReadingSpaceErrorCode.READING_SPACE_NOT_FOUND;
 import static com.sh.threesentences.topic.exception.TopicErrorCode.UNAUTHORIZED_TO_CREATE_TOPIC;
 
+import com.sh.threesentences.common.exception.ForbiddenException;
 import com.sh.threesentences.readingspace.entity.ReadingSpaceMemberRole;
 import com.sh.threesentences.readingspace.repository.UserReadingSpaceRepository;
 import com.sh.threesentences.users.entity.User;
@@ -24,8 +25,8 @@ public class AuthorityService {
     public ReadingSpaceMemberRole getReadingSpaceMemberRole(Long readingSpaceId, String email) {
         User user = userRepository.findByEmail(email)
             .orElseThrow(UserNotFoundException::new);
-        return userReadingSpaceRepository.findByUserAndReadingSpaceId(
-                user, readingSpaceId)
+
+        return userReadingSpaceRepository.findByUserAndReadingSpaceId(user, readingSpaceId)
             .orElseThrow(
                 () -> new IllegalStateException(READING_SPACE_NOT_FOUND.getMessage()));
     }
@@ -39,5 +40,4 @@ public class AuthorityService {
     public void checkUserIsAdminInReadingSpace(Long readingSpaceId, String email) {
         checkRoleIsAdmin(this.getReadingSpaceMemberRole(readingSpaceId, email));
     }
-
 }
